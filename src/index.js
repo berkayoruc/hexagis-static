ccc15736dcc7a257148c87dc3c7b4f372453b1a3
Berkay <berkayoruc@hotmail.com>
2021-01-21 09:48:56 +0300

    leaflet init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-import { bro } from './bro'
-import './styles/main.scss'
+import { bro } from "./bro";
+import "./styles/main.scss";
+// import { L } from 'leaflet'
 
-console.log(bro('lalal'))
+console.log(bro("lalal"));
+var map = L.map("mapid").setView([41, 29], 12);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+L.marker([41, 29])
+  .addTo(map)
+  .bindPopup("Alihan kardeşim, taş devri teknolojileri<br>ve webpack kullanarak tekrar başladım")
+  .openPopup();
